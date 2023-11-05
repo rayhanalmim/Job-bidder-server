966515368d71799849c023b6a520c6e61537ea96
rayhanalmim <rayhanalmim1@gmail.com>
2023-11-05 10:55:48 +0600

    add some jobs data in database and get those data using get method

--- index.js
+++ index.js
@@ -25,6 +25,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const jobsCollection = client.db("JobsBidderHub").collection("jobs");
+
+    app.get('/jobs',async (req, res) =>{
+        const result = await jobsCollection.find().toArray();
+        res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
